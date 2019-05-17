5b52df29897903e9667eba4256a56b38ed86df4b
Thiago Mayllart <thiagomayllart@hotmail.com>
2019-05-17 16:08:55 -0400

    Update domains_db.py

--- database/additional_tools/domains_db.py
+++ database/additional_tools/domains_db.py
@@ -2,29 +2,74 @@
 import os, sys, MySQLdb, time
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
 import credentials
+import config
+
+
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+def enum_sub():
+    # if there is a backup_file do not do recon
+	os.system(os.path.dirname(os.path.abspath(
+		__file__)) + "/../../tools/dependencies/sublister/sublist3r.py -o " + config.path_store + "/" + domain + "/domains-all.txt -d " + domain)
+	time.sleep(2)
+
+	# Subfinder
+	os.system(
+		"subfinder -d " + domain + " -v -o " + config.path_store + "/" + domain + "/domains-subfinder.txt --timeout 6")
+	time.sleep(2)
+
+	# Censys
+	# os.system("python /home/ubuntu/censys-subdomain-finder/censys_subdomain_finder.py --censys-api-id f7c4f30d-df39-4497-b60a-a89e163696ef --censys-api-secret 60WQosDSZT95bI2i42KzwADZDVj2R84X " + domain + " -o /tmp/ICU/"+domain+"/domains-censys.txt")
+	# time.sleep(2)
+
+	# Amass
+	os.system("amass -o " + config.path_store + "/" + domain + "/domains-amass.txt -d " + domain)
+	time.sleep(2)
+
+	# MassDNS
+	os.system(
+		"python $MASSDNS/scripts/subbrute.py $MASSDNS/lists/names.txt " + domain + " | $MASSDNS/bin/massdns -r $MASSDNS/lists/resolvers.txt -t A -o S -w " + config.path_store + "/" + domain + "/domains-massdns.txt")
+	time.sleep(2)
+
+	f = open(config.path_store + "/" + domain + "/domains-massdns.txt", "r")
+	f2 = open(config.path_store + "/" + domain + "/domains-massdns2.txt", "w+")
+	for x in f:
+		line = x.split(". A")
+		f2.write(line[0] + "\n")
+	f.close()
+	f2.close()
 
 try:
 	domain = sys.argv[1].strip()
 	scanId = sys.argv[2]
 
-	if not os.path.exists("/tmp/ICU"):
-		os.makedirs("/tmp/ICU")
+	if not os.path.exists(config.path_store):
+		os.makedirs(config.path_store)
 
-	if not os.path.exists("/tmp/ICU/"+domain+"/"):
-	    os.makedirs("/tmp/ICU/"+domain+"/")
+	if not os.path.exists(config.path_store+"/"+domain+"/"):
+		os.makedirs(config.path_store+"/"+domain+"/")
 
 	#Add new subdomain scanners here. Make sure to let them save the output to /tmp/ICU/{domain}/doains-all.txt
-	os.system(os.path.dirname(os.path.abspath(__file__)) + "/../../tools/dependencies/sublister/sublist3r.py -o /tmp/ICU/"+domain+"/domains-all.txt -d "+domain)
-	time.sleep(2)
 
 	try:
-		#Subfinder
-		os.system("subfinder -d " + domain + " -v -o /tmp/ICU/"+domain+"/domains-subfinder.txt --timeout 6")
-		time.sleep(2)
+		if os.path.exists(config.path_store+"/"+domain+"/backup.txt"):
+			backup_file = open(config.path_store+"/"+domain+"/backup.txt", "r")
+			backup_file_content = backup_file.read()
+			backup_file.close()
+			if backup_file_content.strip() != "":
+				"[+] Restoring Backup!!"
+			else:
+				enum_sub()
+		else:
+			enum_sub()
 
-		#Amass
-		os.system("amass -o /tmp/ICU/"+domain+"/domains-amass.txt -d " + domain)
-        	time.sleep(2)
+
 	except Exception as e:
 		print "An error occured; You probably dont have either subfinder or amass installed. Check the README.md to see you how to install them. Error: "
 		print str(e)
@@ -43,17 +88,52 @@ try:
         topDomainID = [x[4] for x in database_data if x[0] == domain][0]
 
 	#All the domains from the subdomain scanners
-	domains_all = open("/tmp/ICU/"+domain+"/domains-all.txt",'r').read().split('\n')
+	domains_all = open(config.path_store+"/"+domain+"/domains-all.txt",'r').read().split('\n')
+
 
 	try:
 		#Domains from subfinder
-		domains_subfinder = open("/tmp/ICU/"+domain+"/domains-subfinder.txt",'r').read().split('\n')
+		domains_subfinder = open(config.path_store+"/"+domain+"/domains-subfinder.txt",'r').read().split('\n')
 
 	        #Domains from amass
-        	domains_amass = open("/tmp/ICU/"+domain+"/domains-amass.txt",'r').read().split('\n')
+        	domains_amass = open(config.path_store+"/"+domain+"/domains-amass.txt",'r').read().split('\n')
+
+		#Domains from censys
+		#domains_censys = open("/tmp/ICU/"+domain+"/domains-censys.txt",'r').read().split('\n')
+
+		#Domains from subfind3r
+		#domains_sublist3r = open("/tmp/ICU/"+domain+"/domains-sublist3r.txt",'r').read().split('\n')
+
+		#DOmains from Crt
+		#domains_crt = open("/tmp/ICU/"+domain+"/domains-crt.txt",'r').read().split('\n')
+
+		#Domains from massdns
+		domains_massdns = open(config.path_store+"/"+domain+"/domains-massdns2.txt",'r').read().split('\n')
+
+		#Add the massdns domains
+                domains_all.extend(x for x in domains_massdns if x not in domains_all)
+
+                domains_all = list(set(domains_all))
+
+		#Add the sublist3r domains
+                #domains_all.extend(x for x in domains_crt if x not in domains_all)
+
+                #domains_all = list(set(domains_all))
+
+
+		#Add the sublist3r domains
+                #domains_all.extend(x for x in domains_sublist3r if x not in domains_all)
+
+		domains_all = list(set(domains_all))
+
 
 	        #Add the subfinder domains
         	domains_all.extend(x for x in domains_subfinder if x not in domains_all)
+
+		domains_all = list(set(domains_all))
+
+		#Add the censys domains
+		#domains_all.extend(x for x in domains_censys if x not in domains_all)
 
 		#unique
 		domains_all = list(set(domains_all))
@@ -73,37 +153,107 @@ try:
 	#unique -- Unique each time after adding a new list, to limit ram usage
 	domains_all = list(set(domains_all))
 
-	#Put all the online domains in a domains-online.txt
-	os.system(os.path.dirname(os.path.abspath(__file__)) + "/../../tools/online.py /tmp/ICU/"+domain+"/domains-all.txt /tmp/ICU/"+domain+"/domains-online.txt")
+	print domains_all
+	domain_all_write = open(config.path_store+"/"+domain+"/domains-all.txt",'w')
+	for d in domains_all:
+		domain_all_write.write(d+"\n")
 
+	domain_all_write.close()
+
+	#Put all the online domains in a domains-online.txt
+	os.system("sudo python "+os.path.dirname(os.path.abspath(__file__)) + "/../../tools/nmap_masscan.py "+config.path_store+"/"+domain+"/domains-all.txt "+domain)
 	#Convert online domains to array
-	domains_online = open("/tmp/ICU/"+domain+"/domains-online.txt",'r').read().split('\n')
+
+	while ("" in domains_all):
+		domains_all.remove("")
+
+	while ("\n" in domains_all):
+		domains_all.remove("\n")
+
 
 	#Loop through every subdomain
 	for sub_domain in domains_all:
-		#Get the scanID to insert. If the domains was already in the db and isnt changed, then keep the old scanID. otherwise use the scanID of the current scan
-	        insertScanId = scanId if not [x[5] for x in database_data if x[0] == sub_domain] else [x[5] for x in database_data if x[0] == sub_domain][0]
-
-		#If the subdomain is online
-		if sub_domain in domains_online:
-			active=True
-			#If the subdomain used to be offline, give it the current scanID
-			if sub_domain in non_active_subdomains:
+		sub_domain.replace("\n", "")
+		try:
+			if sub_domain:
+				urls_file = open(config.path_store+"/" + domain + "/"+sub_domain+"/domains-online.txt", 'r')
+				urls = urls_file.read()
+				urls_file.close()
+				active = False
+				if urls == "" or urls == None:
+					active = False
+				else:
+					active = True
+				#Get the scanID to insert. If the domains was already in the db and isnt changed, then keep the old scanID. otherwise use the scanID of the current scan
 				insertScanId = scanId
-		else:
-			active=False
+
+				if int(scanId) == 1: #first run
+					counter = 0
+
+					#Insert the new values, or update them if they already existed
+					if active == True:
+
+						nmap_file_f = open(config.path_store+"/" + domain + "/" + sub_domain + "/nmap-ports.txt", "r")
+						nmap_file = nmap_file_f.read()
+						nmap_result = find_between(nmap_file, "\n\n", "\n\n")
+						cursor.execute("INSERT INTO domains (Program, TopDomainID, Active, InScope, Domain, scan_Id, count_new_domain, urls, Nmap_Result) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (program, topDomainID, active, 1, sub_domain, insertScanId, counter, urls, nmap_result))
+						connection.commit()
+						nmap_file_f.close()
+
+						#
+					else:
+						cursor.execute(
+							"INSERT INTO domains (Program, TopDomainID, Active, InScope, Domain, scan_Id, count_new_domain) VALUES (%s, %s, %s, %s, %s, %s, %s) ",
+							(program, topDomainID, active, 1, sub_domain, insertScanId, counter))
+						connection.commit()
+						#
+
+				else:
+					counter = 0
+
+					#select domain_name, if exists, check if is active: counter decays, if becomes active, counter becomes 14
+					#count = heuristics
+
+					#select domain name, if exists, get nmap results, compare old and new results, update nmap results and add the new result to newresult column, update counter
+					if active == True:
+						nmap_file_f = open(config.path_store+"/" + domain + "/" + sub_domain + "/nmap-ports.txt", "r")
+						nmap_file = nmap_file_f.read()
+						nmap_result = find_between(nmap_file, "\n\n", "\n\n")
+						cursor.execute("select Domain from domains where Domain = %s and Active", [sub_domain])
+						domain_was_active = cursor.fetchall()
+						nmap_file_f.close()
+						if len(domain_was_active) == 1:
+
+							cursor.execute(
+								"INSERT INTO domains (Program, TopDomainID, Active, InScope, Domain, scan_Id, urls, Nmap_Result) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE Active = %s, LastModified = now(), scan_Id = %s, count_new_domain = IF(count_new_domain > 0, count_new_domain - 1, count_new_domain), urls = %s, Nmap_Result=%s",
+								(program, topDomainID, active, 1, sub_domain, insertScanId, urls, nmap_result, active, insertScanId, urls, nmap_result))
+							connection.commit()
+
+						#
+						else:
+							cursor.execute(
+								"INSERT INTO domains (Program, TopDomainID, Active, InScope, Domain, scan_Id, count_new_domain, urls, Nmap_Result) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE Active = %s, LastModified = now(), scan_Id = %s,count_new_domain = %s, urls = %s, Nmap_Result=%s",
+								(program, topDomainID, active, 1, sub_domain, insertScanId, 14, urls, nmap_result, active,
+								 insertScanId,14, urls, nmap_result))
+							connection.commit()
+							#
 
 
-		if sub_domain:
-			#Insert the new values, or update them if they already existed
-			cursor.execute("INSERT INTO domains (Program, TopDomainID, Active, InScope, Domain, scan_Id) VALUES (%s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE Active = %s, LastModified = now(), scan_Id = %s", (program, topDomainID, active, 1, sub_domain, insertScanId, active, insertScanId))
-			connection.commit()
+					else:
+						cursor.execute(
+							"INSERT INTO domains (Program, TopDomainID, Active, InScope, Domain, scan_Id, count_new_domain) VALUES (%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE Active = %s, LastModified = now(), scan_Id = %s, urls = null",
+							(program, topDomainID, active, 1, sub_domain, insertScanId, counter, active, insertScanId))
+						connection.commit()
+		except Exception as e:
+			print e
+
 
 	cursor.close ()
 	connection.close ()
 except Exception as e:
 	#Handle the errors, and save them to the database
 	print "error in domains_db.py with main domain; " + domain
+	print e
 	if scanId == "NULL":
 		scanId = None
 	cursor.execute("INSERT INTO errors (Domain, ErrorDescription, Error, Script, scan_Id) VALUES (%s, %s, %s, %s, %s) ", (domain, "error in domains_db.py with main domain; "+domain, e, "sublister_to_db.py", scanId))
