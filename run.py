90c72baa1b4cd52a7adeb6fcaad0eab01d204a8f
Thiago Mayllart <thiagomayllart@hotmail.com>
2019-05-17 16:02:16 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,28 +1,52 @@
 #!/usr/bin/python
 try:
-	import sys, os, MySQLdb, datetime, credentials
+	import sys, os, MySQLdb, datetime, credentials, config
 
-	connection = MySQLdb.connect (host = credentials.database_server, user = credentials.database_username, passwd = credentials.database_password, db = credentials.database_name)
-	cursor = connection.cursor ()
+	sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/tools/")
+	import all_process
+	#if there is a scan running exit
+	if all_process.is_scan_running():
+		print "[+] Scan is still Running, Gonna Try Next time"
+		print "[+] If you want it to stop, kill the process"
+		sys.exit()
+	else:
+		connection = MySQLdb.connect (host = credentials.database_server, user = credentials.database_username, passwd = credentials.database_password, db = credentials.database_name)
+		cursor = connection.cursor ()
 
-	cursor.execute ("insert into scans (StartDate) values (CURRENT_TIMESTAMP)")
-	connection.commit()
-	scanId = cursor.lastrowid
-	cursor.execute ("select Domain from domains where TopDomainID is NULL order by Domain")
-	data = cursor.fetchall ()
-	connection.close()
+		cursor.execute ("insert into scans (StartDate) values (CURRENT_TIMESTAMP)")
+		connection.commit()
+		scanId = cursor.lastrowid
+		#checks if it can increase scanID value, otherwise, keep same value
+		if os.path.exists(config.path_store):
+			scanId -= 1
+		cursor.execute ("select Domain,scan_Id from domains where TopDomainID is NULL order by Domain")
+		data = cursor.fetchall ()
+		connection.close()
 
-	for row in data:
-		print "Starting subdomain scans on " + row[0]
-		os.system("python " + os.path.dirname(os.path.abspath(__file__))  + "/database/additional_tools/domains_db.py " + row[0] + " " + str(scanId))
+		for row in data:
+			scanId_temp = int(scanId)
+			if row[1] == None or int(row[1]) != 0:
+				scanId = 1
+				domain = str(row[0])
+				connection = MySQLdb.connect(host=credentials.database_server, user=credentials.database_username,
+											 passwd=credentials.database_password, db=credentials.database_name)
+				cursor = connection.cursor()
+				cursor.execute("update domains set scan_Id = 0 where Domain = %s", (domain,))
+				connection.commit()
+				connection.close()
+			print "Starting subdomain scans on " + row[0]
+			print "Scan ID: " +str(scanId)
+			os.system("python " + os.path.dirname(os.path.abspath(__file__))  + "/database/additional_tools/domains_db.py " + row[0] + " " + str(scanId))
+			scanId = scanId_temp
 
-	connection = MySQLdb.connect (host = credentials.database_server, user = credentials.database_username, passwd = credentials.database_password, db = credentials.database_name)
-	cursor = connection.cursor ()
-	cursor.execute ("update scans set EndDate = CURRENT_TIMESTAMP where ScanID = %s", (scanId))
-	connection.commit()
-	connection.close()
-
-	os.system("python " + os.path.dirname(os.path.abspath(__file__))  + "/telegram/notify.py " + str(scanId))
+		connection = MySQLdb.connect (host = credentials.database_server, user = credentials.database_username, passwd = credentials.database_password, db = credentials.database_name)
+		cursor = connection.cursor ()
+		cursor.execute ("update scans set EndDate = CURRENT_TIMESTAMP where ScanID = %s", (scanId))
+		os.remove(config.path_store)
+		connection.commit()
+		connection.close()
+		#remove backup file
+		os.system("python " + os.path.dirname(os.path.abspath(__file__))  + "/telegram/notify.py " + str(scanId))
 except Exception, e:
 	print "error: " + str(e)
 
