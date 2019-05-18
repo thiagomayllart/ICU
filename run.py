52a25f701ab860b9e56b159bfaee9ed21ae94403
Thiago Mayllart <thiagomayllart@hotmail.com>
2019-05-18 18:38:10 -0400

    Update run.py

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ try:
 			except Exception as e:
 				print e
 			scanId_temp = int(scanId)
-			if len(row) > 1 and int(row[1]) != 0:
+			if len(row) > 1 and (int(row[1]) != 0 or row[1] == None):
 				scanId = 1
 				domain = str(row[0])
 				connection = MySQLdb.connect(host=credentials.database_server, user=credentials.database_username,
