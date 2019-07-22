80935c875c0db2e4be2596b7a6083c051b67ae6c
Thiago Mayllart <thiagomayllart@hotmail.com>
2019-07-21 21:53:49 -0300

    Update nmap.sh
    Fixed parsing and proxy issues

--- tools/nmap.sh
+++ tools/nmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ports=$(cat $3/$1/$2/masscan-ports.txt | paste -sd "," -)
 domain=$(dig +short $2 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
-nmap --proxies socks4://127.0.0.1:9050 -Pn -n -sT -p $ports $domain | tee $3/$1/$2/nmap-ports.txt
+proxychains nmap -Pn --open -PS -n -sT -p $ports $domain | awk '!/Starting/' | awk '!/Nmap scan/' | awk '!/Host is up/' | awk '!/Proxychains/' | tee $3/$1/$2/nmap-ports.txt
