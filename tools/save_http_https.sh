e91dce7020e5168d4a79ddf72d45490f02f1be72
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-01-13 23:30:38 -0300

    Update save_http_https.sh

--- tools/save_http_https.sh
+++ tools/save_http_https.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat $3/$1/$2/nmap-ports.txt  | grep -P "\b(https?|ssl)\b" | tail -n +2 | cut -d '/' -f1 | tee $3/$1/$2/http_https_ssl.txt
+cat $3/$1/$2/nmap-ports.txt | grep -P "\b(https?|ssl)\b" |  cut -d '/' -f1 | tee $3/$1/$2/http_https_ssl.txt
