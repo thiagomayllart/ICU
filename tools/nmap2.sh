8d9409cdd8acf6995f99274a7bfca1a3b55a6f40
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-01-13 23:30:14 -0300

    Update nmap2.sh

--- tools/nmap2.sh
+++ tools/nmap2.sh
@@ -1,2 +1,2 @@
 domain=$(dig +short $2 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
-proxychains nmap -Pn --open -PS -n -sT -p80,443,8080,8443 $domain | awk '!/Starting/' | awk '!/Nmap scan/' | awk '!/Host is up/' | awk '!/Proxychains/' | tee $3/$1/$2/nmap-ports.txt
+proxychains nmap -Pn --open -PS -n -sT -p80,443,8080,8443 $domain | awk '!/Not shown/' | awk '!/Starting/' | awk '!/Nmap scan/' | awk '!/Host is up/' | awk '!/ProxyChains/' | tee $3/$1/$2/nmap-ports.txt
