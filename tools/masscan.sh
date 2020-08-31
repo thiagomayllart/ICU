0f15d956e57c60a2e9d287f1ba757f75bfa33189
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-08-31 06:59:39 -0300

    Update masscan.sh

--- tools/masscan.sh
+++ tools/masscan.sh
@@ -2,4 +2,4 @@
 echo $3
 echo $2
 echo $1
-sudo masscan $(dig +short $1 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1) -p1-65535 --rate 2000 --wait 3 2> /dev/null | grep -o -P '(?<=port ).*(?=/)' | tee $3/$2/$1/nmap-ports.txt
+sudo masscan $(dig +short $1 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1) -p1-65535 --rate 2000 --wait 3 2> /dev/null | grep -o -P '(?<=port ).*(?=/)' | tee $2/$3/$1/nmap-ports.txt
