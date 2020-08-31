10863cb3a437345b0404e31d6800d30a8a996bdf
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-08-31 06:48:09 -0300

    Update masscan.sh

--- tools/masscan.sh
+++ tools/masscan.sh
@@ -2,4 +2,4 @@
 echo $3
 echo $2
 echo $1
-sudo masscan $(dig +short $1 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1) -p0-10001 --rate 1000 --wait 3 2> /dev/null | grep -o -P '(?<=port ).*(?=/)' | tee $3/$2/$1/masscan-ports.txt
+sudo masscan $(dig +short $1 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1) -p1-65535 --rate 2000 --wait 3 2> /dev/null | grep -o -P '(?<=port ).*(?=/)' | tee $3/$2/$1/masscan-ports.txt
