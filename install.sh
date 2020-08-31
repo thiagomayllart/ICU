75a2bc50068bf73b8476c4020a935c33f6575dd2
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-08-31 06:17:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,9 +21,8 @@ apt-get install nmap
 apt-get install masscan
 
 echo "[+] Downloading wordlists"
-mkdir /usr/share/wordlists
 wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt -o w1.txt
-wget https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt -o resolvers.txt
+wget https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt
 wget https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/subdomains/subdomains.txt -o w2.txt
 cat w1.txt >> w2.txt
 sort w2.txt | uniq > all.txt
