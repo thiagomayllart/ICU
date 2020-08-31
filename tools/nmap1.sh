3cbec14bd1934244fa7f940f6eb39fd74569cac4
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-08-31 06:26:04 -0300

    Update nmap1.sh

--- tools/nmap1.sh
+++ tools/nmap1.sh
@@ -1,2 +1,2 @@
 domain=$(dig +noedns +short $2 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
-nmap -p1-65535 -Pn --open -PS -n -F -sS $domain | awk '!/Not shown/' | awk '!/Starting/' | awk '!/Nmap scan/' | awk '!/Host is up/' | awk '!/Proxychains/' | tee $3/$1/$2/nmap-ports.txt
+nmap -p1-65535 -Pn --open -PS -n -sS $domain | awk '!/Not shown/' | awk '!/Starting/' | awk '!/Nmap scan/' | awk '!/Host is up/' | awk '!/Proxychains/' | tee $3/$1/$2/nmap-ports.txt
