8aed1b7a1b00096eb9f65f0a6deb047404022372
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-08-31 23:06:23 -0300

    Update domains_db.py

--- database/additional_tools/domains_db.py
+++ database/additional_tools/domains_db.py
@@ -29,8 +29,7 @@ def enum_sub():
 	time.sleep(2)
 
 	# Amass
-	os.system("amass enum -brute -w "+os.path.dirname(os.path.abspath(
-		__file__)) +"/../../all2.txt -o " + config.path_store + "/" + domain + "/domains-amass.txt -d " + domain)
+	os.system("amass enum -o " + config.path_store + "/" + domain + "/domains-amass.txt -d " + domain)
 	time.sleep(2)
 
 	os.system("assetfinder " + domain +" -sub-only > "+config.path_store + "/" + domain + "/domains-assetfinder.txt")
