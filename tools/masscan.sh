3f6f3621b1e8f0173b26a8948b2cfbb464a04fd0
Thiago Mayllart <thiagomayllart@hotmail.com>
2020-09-07 13:30:01 -0300

    Update masscan.sh

--- tools/masscan.sh
+++ tools/masscan.sh
@@ -3,4 +3,4 @@ echo $3
 echo $2
 echo $1
 domain=$(dig +noedns +short $2 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
-sudo masscan $domain | head -1) -p1-65535 --rate 300 --wait 3 2> /dev/null | grep -o -P '(?<=port ).*(?=/)' | tee $3/$1/$2/nmap-ports.txt
+sudo masscan $domain -p1-65535 --rate 300 --wait 3 2> /dev/null | grep -o -P '(?<=port ).*(?=/)' | tee $3/$1/$2/nmap-ports.txt
